66570d432dff76f94cea588dd687eb07fadbdd5f
Noel <notasmusikales@gmail.com>
2026-03-26 10:01:11 +0000

    segundo commit

--- modulo_UF188E1/__manifest__.py
+++ modulo_UF188E1/__manifest__.py
@@ -1,3 +1,10 @@
 {
-    'name':
+    'name':'Modulo UF1888E1',
+    'version':'1.0',
+    'depends':['base','contacts'],
+    'data':[
+        'views/res_partner_views.xml'
+    ],
+    'installable': True,
+    'application': False,
 }
